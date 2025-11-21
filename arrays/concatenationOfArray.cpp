bc061bebaa07a8e481fba495b2d5ac18c6a97d60
DantesSin616 <asimpleperson100@gmail.com>
2025-11-21 08:42:25 -0500

    arrays added

---
+++ arrays/concatenationOfArray.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <vector>
+
+using std::vector;
+
+
+vector<int> getConcatenation(vector<int>& nums){
+	for(int i = 0; i < sizeof(nums); i++) {
+		size_t ans = sizeof(nums) * 2;
+		
+	}
+	return ans;
+}
+
+int main(){
+	vector<int> new_vec = {10, 20, 30};
+	vector<int> result = getConcatenation(new_vec);
+
+	return 0;
+}
